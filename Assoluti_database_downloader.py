f00b2f0b0d993a0264bff19fcbbeaf717ba7db21
F-Depi <fdp.federico@gmail.com>
2023-07-12 13:27:10 +0200

    Dealt with typos and events codes for indoor

--- Assoluti_database_downloader.py
+++ Assoluti_database_downloader.py
@@ -4,21 +4,21 @@ from scarp_func import estrazione
 start_time = time.time()
 
 events = []
-with open("event_code.txt", 'r') as f:
+with open("database_events.txt", 'r') as f:
     for line in f:
         event = line.strip()[-2:]
         events.append(event)
-events = events[0:12]
+events = events[20:37]      # selezionare l'intervallo di eventi adeguato, dal file database_events.txt
+
+print(events)
 
 for event in events:
     env = "I"       # I per indoor, P per outdoor
-    sex = "M"       # F per donne, M per uomini
+    sex = "F"       # F per donne, M per uomini
     club = ""       # codice società, lasciare vuoto per tutte le società
     mode = "1"      # 1 per le liste, 2 per le graduatorie
-    event = "28"    # empty for all (will only give first 100 results), different codes for the others. Codes are in event_code.txt
-    folder_path = "database/database_" +sex+ "_" +env+ "_all/raw_data"
-
-estrazione(folder_path, env, sex, club, mode, event)
+    folder_path = "database/database_" +sex+ "_" +env+ "_all/raw_data/Assoluti"
+    estrazione(folder_path, env, sex, club, mode, event)
 
 end_time = time.time()
 execution_time = end_time - start_time
--- scarp_func.py
+++ scarp_func.py
@@ -44,11 +44,13 @@ def get_data_from_url(url, folder_path):
 # These 2 are used to order the data downloaded
 def min2sec(time):
     try:
-        if "h" in time:
+        if time == "5:50:01":                 # database typo in ZAMBELLI Beatrice...
+            total_seconds = 350
+        elif "-" in time:                     # database typo or something weird in jumps
+            total_seconds = 0
+        elif "h" in time:
             hours, minutes, seconds = re.findall(r"(\d+)h(\d+):(\d+)", time)[0]
             total_seconds = int(hours) * 3600 + int(minutes) * 60 + float(seconds)
-        elif time == "5:50:01":                 # database typo in ZAMBELLI Beatrice...
-            total_seconds = 350
         elif ":" in time:
             minutes, seconds = map(float, time.split(":"))
             total_seconds = minutes * 60 + seconds
