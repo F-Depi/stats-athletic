ee478f4e3959109cacfb2ca497b476486605356e
F-Depi <fdp.federico@gmail.com>
2024-02-06 22:04:36 +0100

    minor changes

--- Rankings/Generale/run_me.py
+++ Rankings/Generale/run_me.py
@@ -9,7 +9,7 @@ from manage_file import write_file, read_file
 
 
 anno = '2024';      regione = '';       categoria = ''      
-mese = '';          tipo = '3'
+mese = '3';          tipo = '3'
 
 file_gare = 'Generale/link_gare.csv'
 file_risultati = 'Generale/link_risultati_gare_key.csv'
@@ -29,7 +29,6 @@ if os.path.exists(file_gare):
     print('\nSono stati aggiunti i df_gare\n')
     print(df_gare_new['Codice'])
     
-    #df_gare_new.to_csv(file_gare, mode='a', header=False, sep='\t', index=False)
 #################################################################################################
 
 
@@ -38,7 +37,7 @@ if os.path.exists(file_gare):
 ## aggiorna il DataFrame delle gare controllando se sono disponibili nuovi link nelle gare
 ## con status diverso da 'ok'.
 ## Controlla anche di nuovo tutte le gare con il sigma vecchio, perché in quel caso il numero di
-# link potrebbe aumentare
+## link potrebbe aumentare
 ## Il DataFrame deve essere già del tipo ['Home','Risultati','Versione Sigma','Status']
 df_gare = get_meet_info(df_gare)
 df_gare.to_csv(file_gare, sep='\t', index=False)
@@ -46,54 +45,7 @@ df_gare.to_csv(file_gare, sep='\t', index=False)
 
 
 
-################# Prendiamo i risultati che ci sono nei link e salviamoli ########################
-## Questa è la parte più ostica. Ogni link potrebbe avere typo, colonne con nomi diversi. etc.
-## Di solito bisogna runnare il codice più volte e modificare le funzioni in scraping_risultati.py
-## in base agli errori che saltano fuori.
-""" links = read_file(file_link); l=len(links)
-header_written = False # we need to write the header once
-for i, link in enumerate(links):
-    print('\nLink '+str(i+1)+'/'+str(l)+': '+link)
-    data = results_from_sigma(link)
-    data['Disciplina'] = Disciplina
-    data = data[['Prestazione', 'Atleta', 'Cat.', 'Anno', 'Società', 'Disciplina', 'Gara']]
-    mode = 'a' if header_written else 'w'
-    data.to_csv(file_results, mode=mode, index=False, header=not header_written)
-    header_written = True  # Set to True after writing the header once """
-##################################################################################################
+################# Otteniamo i link a ogni risultato di ogni disciplina per ogni gara #############
 
-
-
-################# Prendiamo i dati dal csv e tiriamo fuori i ranking    ##########################
-""" data = pd.read_csv(file_results)
-    
-data = data.sort_values(by='Prestazione') # Sort the DataFrame based on the 'Prestazione' column
-data = data.drop_duplicates(subset='Atleta', keep='first') # only keeps the best result for each athlete
-data.to_csv(file_rankings, index=False) """
-
-
-
-""" 
-# Otteniamo direttamente la tabellona senza passare per il file (più veloce ma si presta meno al trial and error)
-data = []
-links = read_file(file_link)
-for link in links:
-    #print(link)
-    data1 = results_from_sigma(link)
-    data.append(data1)
-
-data = pd.concat(data).reset_index(drop=True)
-print(data)
-
-# Rankings
-output_file3 = 'rank__2024_01_60HS_PRO.csv'
-data['Prestazione'] = pd.to_numeric(data['Prestazione'], errors='coerce') # Convert 'Prestazione' column to numeric, errors='coerce' will convert non-numeric values to NaN
-data = data.dropna(subset=['Prestazione']) # Drop rows with NaN values in the 'Prestazione' column
-data = data.sort_values(by='Prestazione') # Sort the DataFrame based on the 'Prestazione' column
-data = data.drop_duplicates(subset='Atleta', keep='first') # only keeps the best result for each athlete
-data = data.reset_index(drop=True)
-
-data.to_csv(output_file3)   
- """
 
 
